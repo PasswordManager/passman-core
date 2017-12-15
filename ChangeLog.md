a72cc0761ed31577ddaf917c7eaf2d3e18a88b25
Matthew Harm Bekkema <mbekkema97@gmail.com>
2017-12-15 20:06:56 +1100

    Export version number in Passman.Core.Version

--- ChangeLog.md
+++ ChangeLog.md
@@ -1,3 +1,7 @@
+## 0.2.0.0 (unreleased)
+
+* Export version number in Passman.Core.Version
+
 ## 0.1.0.0
 
 * Initial release
---
+++ src/Passman/Core/Version.hs
@@ -0,0 +1,30 @@
+-- Copyright (C) 2017  Matthew Harm Bekkema
+--
+-- This file is part of passman-core
+--
+-- passman-core is free software: you can redistribute it and/or modify
+-- it under the terms of the GNU General Public License as published by
+-- the Free Software Foundation, either version 3 of the License, or
+-- (at your option) any later version.
+--
+-- passman-core is distributed in the hope that it will be useful,
+-- but WITHOUT ANY WARRANTY; without even the implied warranty of
+-- MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+-- GNU General Public License for more details.
+--
+-- You should have received a copy of the GNU General Public License
+-- along with this program.  If not, see <https://www.gnu.org/licenses/>.
+
+-----------------------------------------------------------------------------
+-- |
+-- Module      : Passman.Core.Version
+-- Copyright   : Matthew Harm Bekkema 2017
+-- License     : GPL-3
+-- Maintainer  : mbekkema97@gmail.com
+-----------------------------------------------------------------------------
+
+module Passman.Core.Version
+    ( version
+    ) where
+
+import Paths_passman_core (version)
